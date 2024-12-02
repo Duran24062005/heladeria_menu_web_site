7ce34ad233cb2ca53343f93a8164268c4a8fcd29
Alexi Dg <alexisdurangomez588@gmail.com>
2024-12-02 12:56:07 -0500

    "changes N#3"

--- scripts.js
+++ scripts.js
@@ -2,6 +2,11 @@ const menuItems = [
     {
         name: 'Cono de Helado',
         description: "3 bolas de helado por 7.500 mil, 2 bolas de helado por 4.000 mil",
+        image: '/assets/four-scoops-ice-cream.jpg'
+    },
+    {
+        name: 'Limonada cerezada',
+        description: "3 bolas de helado por 7.500 mil, 2 bolas de helado por 4.000 mil",
         image: '/assets/vista-frontal-color-rojo-cereza-coctel-pajitas-dentro-pequena-lata-enfriamiento-fresco-escritorio-madera-junto-cerezas-frescas-flores-escritorio-rosa.png'
     },
     {
@@ -10,54 +15,54 @@ const menuItems = [
         image: '/assets/1000_F_312360554_CD4jAXwx5hKMnNFCZktgDaHWrF0DJO8M.jpg'
     },
     {
-        name: 'Sundae',
+        name: 'Canasta',
         description: "3 bolas de helado por 10.000 mil",
         image: '/assets/1000_F_50866103_nga6ooBmpWVNnsdrqqH5uDxf2xnUwq3a.jpg'
     },
     {
-        name: 'Bowl de Helado',
+        name: 'Copa Fruta',
         description: "3 bolas de helado con barquillos de chocolate por 10.000 mil",
-        image: '/assets/1000_F_1071069985_o3cxq0R3KWbmlyZkKR0UZbWQOtr6jDot.jpg'
+        image: '/assets/1000_F_55362648_xzL2X1RYeZACfIJSoZuuBV87QAetfNNT.jpg'
     },
     {
-        name: 'Sándwich de Helado',
+        name: 'Ensalada de frutas',
         description: "Brownie con una bola de helado y salsas por 10.000 mil",
+        image: '/assets/1000_F_312360554_CD4jAXwx5hKMnNFCZktgDaHWrF0DJO8M.jpg'
+    },
+    {
+        name: 'Milo Frappe',
+        description: "Banano con 3 bolas de helado, crema chantilly y salsa de chocolate por 7.500 mil",
         image: '/assets/1000_F_472583524_OUMweeIXxuqFptsf9AVEYaTTnbnUbQ2g.jpg'
     },
     {
-        name: 'Flotante de Helado',
-        description: "Banano con 3 bolas de helado, crema chantilly y salsa de chocolate por 7.500 mil",
-        image: 'https://via.placeholder.com/200x200?text=Flotante'
-    },
-    {
-        name: 'Parfait de Helado',
+        name: 'Ensalada de frutas 2',
         description: "Licuado de helado con leche y crema chantilly, pequeño por 8.500 mil, grande por 12.000 mil",
-        image: 'https://via.placeholder.com/200x200?text=Parfait'
+        image: '/assets/1000_F_1071069985_o3cxq0R3KWbmlyZkKR0UZbWQOtr6jDot.jpg'
     },
     {
-        name: 'Torta de Helado',
+        name: 'Helado Brawnie',
         description: "Creps con una bola de helado y salsa de frutas por 10.000 mil",
-        image: 'https://via.placeholder.com/200x200?text=Torta'
+        image: '/assets/brownie-sundae-with-scoop-vanilla-ice-cream.jpg'
     },
     {
-        name: 'Cóctel de Helado',
+        name: 'Helado Barquillo',
         description: "Limonada cerezada con leche condensada por 14.000 mil",
-        image: 'https://via.placeholder.com/200x200?text=Coctel'
+        image: '/assets/chocolate-ice-cream-dessert.jpg'
     },
     {
         name: 'Batido de Helado',
         description: "Granizado de Milo con salsa de chocolate y crema chantilly, pequeño por 8.500 mil, grande por 12.000 mil",
-        image: 'https://via.placeholder.com/200x200?text=Batido'
+        image: '/assets/creamy-vanilla-milkshake-with-chocolate-sauce-white-saucer.jpg'
     },
     {
-        name: 'Smoothie de Helado',
+        name: 'Banana Split',
         description: "3 bolas de helado con galleta de chocolate normal por 7.500 mil, con fruta por 9.000 mil",
-        image: 'https://via.placeholder.com/200x200?text=Smoothie'
+        image: '/assets/ice-cream-dessert-with-banana.jpg'
     },
     {
-        name: 'Paleta de Helado',
+        name: 'Copa Galleta',
         description: "Helado servido en vaso, grande por 6.000 mil, mediano por 3.500 mil",
-        image: 'https://via.placeholder.com/200x200?text=Paleta'
+        image: '/assets/three-color-icecream-gelato-buns-glass.jpg'
     }
 ];
 
