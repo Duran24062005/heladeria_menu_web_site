2904d7e86bef5939981146f069c207bb8b14a795
Alexi Dg <alexisdurangomez588@gmail.com>
2024-12-12 14:34:31 -0500

    "updates in cards, add prices for everyone."

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 const menuItems = [
     {
         name: 'Cono de Helado',
-        description: "3 bolas de helado por 7.500 mil, 2 bolas de helado por 4.000 mil",
+        description: "Bolas de helado.",
+        price: "3 por: $7.500 o 2 por: $4.000",
         image: '/assets/four-scoops-ice-cream.jpg'
     },
     {
         name: 'Limonada cerezada',
-        description: "Licuado de limón y cerezas con leche condesada. 14.000mil",
+        description: "Licuado de limón y cerezas con leche condesada.",
+        price: "Por: $14.000",
         image: '/assets/vista-frontal-color-rojo-cereza-coctel-pajitas-dentro-pequena-lata-enfriamiento-fresco-escritorio-madera-junto-cerezas-frescas-flores-escritorio-rosa.png'
     },
     // {
@@ -16,57 +18,68 @@ const menuItems = [
     // },
     {
         name: 'Canasta',
-        description: "3 bolas de helado por 10.000 mil",
+        description: "3 bolas de helado.",
+        price: "Por: $10.000",
         image: '/assets/1000_F_50866103_nga6ooBmpWVNnsdrqqH5uDxf2xnUwq3a.jpg'
     },
     {
         name: 'Copa Fruta',
-        description: "2 bola y media con fruta variada 12.000mil",
+        description: "2 bola y media con fruta variada.",
+        price: "Por: $12.000",
         image: '/assets/1000_F_55362648_xzL2X1RYeZACfIJSoZuuBV87QAetfNNT.jpg'
     },
     {
         name: 'Ensalada de frutas',
-        description: "Fruta variada con una bola de helado, queso, leche condensada y crema chantilly. 15.000mil",
+        description: "Fruta variada con una bola de helado, queso, leche condensada y crema chantilly.",
+        price: "Por: $15.000",
         image: '/assets/1000_F_312360554_CD4jAXwx5hKMnNFCZktgDaHWrF0DJO8M.jpg'
     },
     {
         name: 'Milo Frappe',
-        description: "Granizado de Milo con salsa de chocolate y crema chantilly. Pequeño 8.500 Grande 12.000mil",
+        description: "Granizado de Milo con salsa de chocolate y crema chantilly. Pequeño y Grande",
+        price: "Por: $8.500 o $12.000",
         image: '/assets/1000_F_472583524_OUMweeIXxuqFptsf9AVEYaTTnbnUbQ2g.jpg'
     },
     {
         name: 'Helado Vaso',
-        description: "Grande: 6.000mil Mediano: 3.500",
+        description: "Grande y Mediano.",
+        price: "Por: $6.000 o $3.500",
         image: '/assets/delicious-chocolate-ice-cream-cup.jpg'
     },
     {
         name: 'Helado Brawnie',
-        description: "Brawnie con una bola de helado y salsas. 10.000mil",
+        description: "Brawnie con una bola de helado y salsas.",
+        price: "Por: $10.000",
         image: '/assets/brownie-sundae-with-scoop-vanilla-ice-cream.jpg'
     },
     {
         name: 'Helado Barquillo',
-        description: "3 bolas de helado con barquillos de chocolate. 10.000mil",
+        description: "3 bolas de helado con barquillos de chocolate.",
+        price: "Por: $10.000",
         image: '/assets/chocolate-ice-cream-dessert.jpg'
     },
     {
         name: 'Batido de Helado',
-        description: "Licuado de helado con leche con crema chantilly. Pequeño: 8.500 grande: 12.000mil",
+        description: "Licuado de helado con leche con crema chantilly. Pequeño y grande",
+        price: "Por: $8.500 o $12.000",
         image: '/assets/creamy-vanilla-milkshake-with-chocolate-sauce-white-saucer.jpg'
     },
     {
         name: 'Banana Split',
-        description: "Banano con 3 bolas de helado y crema chantilly con salsa de chocolate. 7.500",
+        description: "Banano con 3 bolas de helado y crema chantilly con salsa de chocolate.",
+        price: "Por: $7.500",
         image: '/assets/ice-cream-dessert-with-banana.jpg'
     },
     {
         name: 'Copa Galleta',
-        description: "3 bolas de helado con galleta de chocolate Normal: 7.500 Con fruta: 9.000mil",
+        description: "3 bolas de helado con galleta de chocolate normal o con fruta.",
+        price: "Por: $7.500 o $9.000",
         image: '/assets/three-color-icecream-gelato-buns-glass.jpg'
     },
     {
         name: 'Helado creps',
-        description: "Creps con una bola de helado y salsa de frutas por 10.000 mil",
+        description: "Creps con una bola de helado y salsa de frutas.",
+        price: "Por: $10.000",
         image: '/assets/pancakes-with-chocolate-jam-berries-tasty-breakfast-flat-lay-top-view (1).jpg'
     }
 ];
@@ -78,11 +91,20 @@ function createMenuItem(item) {
     div.className = 'bg-white rounded-lg overflow-hidden shadow-lg shadow-pink-700 flex flex-col h-full';
     div.innerHTML = `
         <div class="relative h-48">
-            <img src="${item.image}" alt="${item.name}" class="w-full h-full object-cover data-twe-lazy-load-init">
+            <img src="${item.image}" alt="${item.name}" class="w-full h-full object-cover">
         </div>
-        <div class="p-2 flex-grow flex flex-col justify-between">
+        <div class="p-4 flex-grow flex flex-col justify-between">
             <h3 class="text-lg font-semibold text-center mb-2">${item.name}</h3>
-            <p class="text-sm text-center">${item.description}</p>
+            <p class="text-sm text-center mb-4">${item.description}</p>
+            <div class="text-center relative h-16 mt-auto">
+                <div class="absolute inset-0 flex items-center justify-center">
+                    <span class="shadow-lg shadow-pink-700 p-2 bg-gradient-to-t from-cyan-100 to-pink-400 rounded-lg transform -rotate-12 flex items-center justify-center">
+                        <span class="text-white font-bold text-xs sm:text-sm whitespace-nowrap px-2">
+                            ${item.price}
+                        </span>
+                    </span>
+                </div>
+            </div>
         </div>
     `;
     return div;
